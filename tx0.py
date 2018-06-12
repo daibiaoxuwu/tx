47c9ef5cf835f963c1274153c74601ab630e7184
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2018-06-13 00:38:21 +0800

    修改：     tx0.py 尝试不同loss. 	修改：     tx12.py debug:最重要的,原来summ没有累加起来.以及边界处理,把loss两处计算分离出来.

--- tx0.py
+++ tx0.py
@@ -1,27 +1,39 @@
 import cv2
 import numpy as np
 
-picin = cv2.imread('2.png')
+picin = cv2.imread('1.jpg')
 print(picin.shape)
 print(picin[100][100])
 print(picin[100][101])
 print(picin[100][102])
-loss=np.zeros(picin.shape)
-summ=np.zeros(picin.shape[:2])
-last=np.zeros(picin.shape[:2])
+loss=np.zeros(picin.shape,dtype='ubyte')
+summ=np.zeros(picin.shape[:2],dtype='int32')
+last=np.zeros(picin.shape[:2],dtype='int32')
 
+for i in range(picin.shape[0]-1):
+    for j in range(picin.shape[1]-1):
+        diff=0
+        '''
+        for r in range(3):
+            diff+=abs(picin[i+1][j][r]-picin[i][j][r])/6/255
+            '''
+#        for r in range(3):
+#            diff+=int(abs(int(picin[i][j+1][r])-int(picin[i][j][r])))
+#        for r in range(3):
+#            diff+=int(abs(int(picin[i+1][j][r])-int(picin[i][j][r])))
+        #diff=abs(picin[i+1][j][0]+picin[i+1][j][1]+picin[i+1][j][2]-picin[i][j][0]-picin[i][j][1]-picin[i][j][2])/3/255
+        #diff=abs(picin[i+1][j][0]+picin[i+1][j][1]+picin[i+1][j][2]-picin[i][j][0]-picin[i][j][1]-picin[i][j][2])/3/255
+        diff=abs(int(picin[i][j+1][0])+int(picin[i][j+1][1])+int(picin[i][j+1][2])-int(picin[i][j][0])-int(picin[i][j][1])-int(picin[i][j][2]))
+        #diff=abs(int(picin[i+1][j][0])+int(picin[i+1][j][1])+int(picin[i+1][j][2])-int(picin[i][j][0])-int(picin[i][j][1])-int(picin[i][j][2]))
+        loss[i][j][0]+=(diff)
+        loss[i][j][1]+=(diff)
+        loss[i][j][2]+=(diff)
+
+'''
 for i in range(picin.shape[0]-1):
     for j in range(picin.shape[1]):
         for r in range(3):
-            diff=(picin[i+1][j][r]-picin[i][j][r])/3/255
-            loss[i][j][0]+=abs(diff)
-            loss[i][j][1]+=abs(diff)
-            loss[i][j][2]+=abs(diff)
-
-for i in range(picin.shape[0]-1):
-    for j in range(picin.shape[1]):
-        for r in range(3):
-            if 
+'''         
 
 
 #cv2.imshow('pic',picin)
--- tx12.py
+++ tx12.py
@@ -7,25 +7,62 @@ print(picin[100][100])
 print(picin[100][101])
 print(picin[100][102])
 loss=np.zeros(picin.shape[:2])
+disp=np.zeros(picin.shape)
 
-for i in range(picin.shape[0]):
-    for j in range(picin.shape[1]-1):
+
+def diff(picin,i,j):
+    '''
         for r in range(3):
-            loss[i][j]+=abs(int(picin[i][j+1][r])-int(picin[i][j][r]))/3/255        #每个loss是0到1的
+            diff+=int(abs(int(picin[i][j+1][r])-int(picin[i][j][r])))
+        for r in range(3):
+            diff+=int(abs(int(picin[i+1][j][r])-int(picin[i][j][r])))
+    '''
+    did=abs(int(picin[i][j+1][0])+int(picin[i][j+1][1])+int(picin[i][j+1][2])-int(picin[i][j][0])-int(picin[i][j][1])-int(picin[i][j][2]))
+    return did
+
+
+
+for i in range(picin.shape[0]-1):
+    for j in range(picin.shape[1]-1):#loss的最后一行最后一列为0
+        loss[i][j]=diff(picin,i,j)
+
+for i in range(picin.shape[0]-1):   #修正最后一列
+    loss[i][picin.shape[1]-1]=loss[i][picin.shape[1]-2]
+
 
 for step in range(picin.shape[0]-10):
-    summ=np.ones(picin.shape[:2])*10000                 #重新初始化sum
-    last=np.zeros(picin.shape[:2],dtype='int32')
+    summ=np.ones(picin.shape[:2])*10000               #重新初始化sum
+    last=np.ones(picin.shape[:2],dtype='int32')*1000000
 
-    for i in range(picin.shape[0]-1):
-        for j in range(1,picin.shape[1]-1-step):
-            for r in range(-1,2):
-                if loss[i][j+r]<summ[i+1][j]:       #取三个的最小值
-                    summ[i+1][j]=loss[i][j+r]
-                    last[i+1][j]=j+r                #存放上面一排的位置
+    for j in range(picin.shape[1]-step):    #范围:全
+        summ[0][j]=loss[0][j]
+    for i in range(picin.shape[0]-1):               #范围:i+1顶到头 最后一行是有summ和last的 第一行没有
+        for j in range(picin.shape[1]-step):    #范围:全
+            for r in range(max(j-1,0),min(j+2,picin.shape[1]-1-step)):
+                if summ[i][r]<summ[i+1][j]:       #取三个的最小值
+                    summ[i+1][j]=summ[i][r]
+                    last[i+1][j]=r                #存放上面一排的位置
             summ[i+1][j]+=loss[i+1][j]
-    minsum=10000
-    for j in range(1,picin.shape[1]-1-step):
+            
+
+    '''
+        maxs=max(summ[i+1][:picin.shape[1]-step])
+        print('max',maxs,max(loss[i+1]),i)
+        for j in range(picin.shape[1]-step):
+            for r in range(3):
+                if summ[i+1][j]<10:
+                    disp[i+1][j][r]=0
+                else:
+                    disp[i+1][j][r]=1
+                disp[i+1][j][r]=summ[i+1][j]/maxs
+    cv2.imwrite('1g.jpg',disp)
+    cv2.waitKey()
+                    '''
+
+                
+                
+    minsum=10000000000
+    for j in range(1,picin.shape[1]-1-step):        #因为第一列和最后一列没有,范围缩小,缩小掉黑边
         if summ[picin.shape[0]-1][j]<minsum:
             minsum=summ[picin.shape[0]-1][j]
             oldm=j
@@ -34,6 +71,7 @@ for step in range(picin.shape[0]-10):
 
 #显示图片
     for i in range(picin.shape[0]-1,-1,-1):
+        print('mp33',i,minpos)
         for r in range(3):
             picin[i][minpos][r]=0
         minpos=last[i][minpos]
@@ -42,7 +80,7 @@ for step in range(picin.shape[0]-10):
 
 
     minpos=oldm
-    for i in range(picin.shape[0]-1,-1,-1):
+    for i in range(picin.shape[0]-1,-1,-1):             #从最底下一直到最上面
         for j in range(minpos,picin.shape[1]-1-step):   #一行的像素左移
             for r in range(3):
                 picin[i][j][r]=picin[i][j+1][r]
@@ -54,13 +92,11 @@ for step in range(picin.shape[0]-10):
 
 #    input()
     minpos=oldm
-    loss2=loss[:]#debug
-
-    for i in range(picin.shape[0]-1,-1,-1):
-        for j in range(minpos-1,minpos):
-            loss[i][j]=0
-            for r in range(3):
-                loss[i][j]+=abs(int(picin[i][j+1][r])-int(picin[i][j][r]))/3/255  #重新计算loss 
+#    loss2=loss[:]#debug
+    #更新loss
+    for i in range(picin.shape[0]-2,-1,-1):             #i不能从头到尾了因为计算loss i最后一行没有
+        for j in range(minpos-1,minpos):                #j其实是一个数
+            loss[i][j]=diff(picin,i,j)
         minpos=last[i][minpos]                         #更新minpos
 #        print('mp:',minpos)
 
