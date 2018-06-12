c0463a649392ff5a2fa4f17820e4ecc56c44d8c2
daibiaoxuwu <dujl16@mails.tsinghua.edu.cn>
2018-06-12 17:26:32 +0800

    修改：     tx12.py 只计算当地的loss加快速读

--- tx12.py
+++ tx12.py
@@ -10,12 +10,12 @@ loss=np.zeros(picin.shape[:2])
 summ=np.ones(picin.shape[:2])*10000
 last=np.zeros(picin.shape[:2],dtype='int32')
 
+for i in range(picin.shape[0]):
+    for j in range(picin.shape[1]-1):
+        for r in range(3):
+            loss[i][j]+=abs(picin[i][j+1][r]-picin[i][j][r])/3/255
+
 for step in range(picin.shape[0]):
-    for i in range(picin.shape[0]):
-        for j in range(picin.shape[1]-1-step):
-            for r in range(3):
-                diff=(picin[i][j+1][r]-picin[i][j][r])/3/255
-                loss[i][j]+=abs(diff)
 
     for i in range(picin.shape[0]-1):
         for j in range(1,picin.shape[1]-1-step):
@@ -31,14 +31,20 @@ for step in range(picin.shape[0]):
             minpos=j
 
     for i in range(picin.shape[0]-1,-1,-1):
-        for j in range(minpos,picin.shape[1]-1-step):
+        for j in range(minpos,picin.shape[1]-1-step):   #一行的像素左移
             for r in range(3):
                 picin[i][j][r]=picin[i][j+1][r]
-            
+            loss[i][j]=picin[i][j+1]                    #loss左移
         for r in range(3):
-            picin[i][picin.shape[1]-1-step][r]=0
+            picin[i][picin.shape[1]-1-step][r]=0        #最左边抹黑
+        minpos+=last[i][minpos]                         #更新minpos
 
-        minpos+=last[i][minpos]
+    for i in range(picin.shape[0]-1,-1,-1):
+        for j in range(minpos-1,minpos+2):
+            for r in range(3):
+                loss[i][j]+=abs(picin[i][j+1][r]-picin[i][j][r])/3/255  #重新计算loss 
+        minpos+=last[i][minpos]                         #更新minpos
+
     cv2.imwrite('1'+str(step)+'.jpg',picin)
     print(step)
 
